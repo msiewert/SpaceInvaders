b9e22311aea716ee386811bb0d210bd1cc1c4515
Edward Siewert <edwarddalesiewert@gmail.com>
2026-03-09 20:43:20 -0600

    grid placed

--- Assets/scripts/Invaders.cs
+++ Assets/scripts/Invaders.cs
@@ -11,10 +11,15 @@ public class Invaders : MonoBehaviour
     {
         for (int row = 0; row < this.rows; row++)
         {
+            Vector3 rowPosition = new Vector3(0.0f, row * 2.0f, 0.0f);
             for (int column = 0; column < this.columns; column++)
             {
                 Invader invader = Instantiate(this.prefabs[row], this.transform);
+                Vector3 position = rowPosition;
+                position.x += column * 2.0f;
+                invader.transform.position = position;
             }
+
         }
     }
 
